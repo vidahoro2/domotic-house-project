8424eef01c0b1b9a20e42232454b597ee9ba0de6
vidahoro2 <vidahoro@gmail.com>
2023-03-29 17:42:05 -0500

    cambios en loginUser

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { LoginUser } from './components/LoginUser';
 import { LoginUserPage } from './pages/loginUserPage';
+import React from 'react';
 
 
 function App() {
