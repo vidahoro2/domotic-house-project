836f99c82c347bd19e47f3bfc27e15c3cff4e5b4
MiguelAQuevedoP <mquevedo@unicauca.edu.co>
2023-05-07 13:11:46 -0500

    CRUD added

--- src/server/controllers/user.controllers.js
+++ src/server/controllers/user.controllers.js
@@ -1,31 +1,73 @@
-import { pool } from '../db.js';
-import { encryptPassword, matchPassword } from '../lib/helpers.js';
+import { pool } from "../db.js";
+import { encryptPassword, matchPassword } from "../lib/helpers.js";
 
-export const getUsers = (req, res) => {
-    console.log('getUsers');
-}
-export const getUser = (req, res) => {
-    console.log('getUser');
-}
+export const getUsers = async (req, res) => {
+  try {
+    const [result] = await pool.query("SELECT * FROM users");
+    res.json(result);
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+export const getUser = async (req, res) => {
+  try {
+    const [result] = await pool.query("SELECT * FROM users WHERE id = ?", [
+      req.params.id,
+    ]);
+    if (result.length === 0) {
+      return res.status(404).json({ message: "User not found" });
+    } else {
+      res.json(result[0]);
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
 export const insertUser = async (req, res) => {
-    let {fullname, address, phone, email, username, password} = req.body;
+  try {
+    let { fullname, address, phone, email, username, password } = req.body;
     password = await encryptPassword(req.body.password);
-    
-    const [result]  = await pool.query('INSERT INTO users (fullname, address, phone, email, username, password) values (?, ?, ?, ?, ?, ?)',[fullname,address,phone,email,username,password]);
+
+    const [result] = await pool.query(
+      "INSERT INTO users (fullname, address, phone, email, username, password) values (?, ?, ?, ?, ?, ?)",
+      [fullname, address, phone, email, username, password]
+    );
     res.json({
-        id:result.insertId,
-        fullname,
-        address,
-        phone,
-        email,
-        username,
-        password
+      id: result.insertId,
+      fullname,
+      address,
+      phone,
+      email,
+      username,
+      password,
     });
-    
-}
-export const updateUser = (req, res) => {
-    console.log('updateUser');
-}
-export const deleteUser = (req, res) => {
-    console.log('deleteUser');
-}
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+export const updateUser = async (req, res) => {
+  try {
+    //const {fullname, address, phone, email,username, password} = req.body;
+
+    const result = await pool.query("UPDATE users SET ? WHERE id = ?", [
+      req.body,
+      req.params.id,
+    ]);
+
+    res.json(result);
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+export const deleteUser = async (req, res) => {
+  try {
+    const [result] = await pool.query("DELETE FROM users WHERE id = ?", [
+      req.params.id,
+    ]);
+    if (result.affectedRows === 0)
+      return res.status(404).json({ message: "User not found" });
+    return res.status(204).json({ message: "User deleted" });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
