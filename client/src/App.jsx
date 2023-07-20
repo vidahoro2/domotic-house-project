42193e24f247b470528cf68bbfba629269e62eeb
vidahoro2 <vidahoro@gmail.com>
2023-07-19 21:33:05 -0500

    rutas actualizadas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,9 @@ import './App.css'
 import {LoginUserPage} from './pages/LoginUserPage'
 import { LandingPage } from './pages/LandingPage'
 import { RegisterFormPage } from './pages/RegisterFormPage'
+import { Services } from './pages/Services'
+import {RoomSelection} from './pages/RoomSelection'
+import {ModifyData} from './pages/ModifyData'
 import ReactDOM from 'react-dom/client'
 import { Routes, Route } from 'react-router-dom'
 
@@ -13,6 +16,9 @@ function App() {
         <Route path='/' exaxt element={<LandingPage/>}/>
         <Route path='/login' exaxt element={<LoginUserPage/>}/>
         <Route path='/logup' exaxt element={<RegisterFormPage/>}/>
+        <Route path='/services' exaxt element={<Services/>} />
+        <Route path='room-selection' exaxt element={<RoomSelection/>}/>  
+        <Route path='modify-data' exaxt element={<ModifyData/>}/>
       </Routes>
     </>
   )
